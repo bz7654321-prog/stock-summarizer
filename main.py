52c6d5fa60b0a28fa10e8699752d16aa7a9db0a1
bz7654321-prog <bz7654321@gmail.com>
2026-04-30 21:36:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ TARGET_STOCKS_BY_CHANNEL = {
 
 YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
 
-LOOKBACK_HOURS = int(os.environ.get("LOOKBACK_HOURS", "24"))
+LOOKBACK_HOURS = int(os.environ.get("LOOKBACK_HOURS", "12"))
 MAX_VIDEOS_PER_CHANNEL = int(os.environ.get("MAX_VIDEOS_PER_CHANNEL", "10"))
 GEMINI_MODEL = os.environ.get("GEMINI_MODEL", "gemini-2.5-flash-lite")
 
